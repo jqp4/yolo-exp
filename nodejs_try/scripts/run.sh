5c5672f487622db544e160d18e20fa83ba86e9ac
Gleb <skryabin.gleb.d@gmail.com>
2023-05-09 09:33:20 +0300

    removed part with node js

--- nodejs_try/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-# echo "2 from sh script:"
-# echo $1
-
-cd ./scripts
-
-# output.json --> jsonGraphData.js
-python3 json2js.py output.json ../public/algoviewCodeCopy/data/jsonGraphData.js
-
-echo "done!"
